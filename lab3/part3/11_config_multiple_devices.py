dee42568df003d7cbca1bfe2f6996a8360f2cf77
QuynhHuongVuPXL <quynhhuong.vu@student.pxl>
2026-01-04 17:16:58 +0100

    ran all the python scripts to make sure they work

--- lab3/part3/11_config_multiple_devices.py
+++ lab3/part3/11_config_multiple_devices.py
@@ -4,28 +4,26 @@ from netmiko import ConnectHandler
 devices = [
     {
         "device_type": "cisco_xe",
-        "ip": "192.168.56.101",     # your real CSR1000v
+        "ip": "192.168.56.101",    
         "username": "cisco",
         "password": "cisco123!",
         "secret": "cisco123!",
     },
     {
         "device_type": "cisco_xe",
-        "ip": "192.168.56.102",     # fake device (example)
+        "ip": "192.168.56.102",     
         "username": "admin",
         "password": "password",
         "secret": "password",
     }
 ]
 
-# Configuration commands sent to each device
 config_commands = [
     "interface Loopback200",
     "ip address 200.200.200.1 255.255.255.255",
     "description Configured_on_multiple_devices",
 ]
 
-# Loop through devices and send config
 for dev in devices:
     print(f"\n----- Connecting to {dev['ip']} -----")
 
--- lab3/part3/9_conditional_if_else.py
+++ lab3/part3/9_conditional_if_else.py
@@ -12,19 +12,16 @@ net_conn = ConnectHandler(**device)
 net_conn.enable()
 
 desired_hostname = "MY_ROUTER"
-
 output = net_conn.send_command("show running-config | include hostname")
-
 print("Current hostname:", output)
 
 if desired_hostname in output:
     print("\nHostname is already correct. No changes needed.")
 else:
     print("\nHostname is incorrect. Fixing it now...")
-    config_cmd = [f"hostname {desired_hostname}"]
-    config_output = net_conn.send_config_set(config_cmd)
-    print(config_output)
-    net_conn.save_config()
+    print(net_conn.send_config_set([f"hostname {desired_hostname}"]))
+
+    print(net_conn.send_command("write memory", expect_string=r"#"))
     print("Hostname updated and configuration saved.")
 
 net_conn.disconnect()
